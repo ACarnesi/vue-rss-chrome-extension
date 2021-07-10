037ffc80b0fc9b37676e1f02ef032ff458f52624
Andrew Carnesi <andrew.carnesi@outlook.com>
2021-07-09 21:19:04 -0500

    Added vuex and vue-router through cli instead

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
 <style>
-  body {
-    background-color: #111;
-  }
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #678fb8;
-    margin-top: 60px;
-  }
-</style>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+body {
+  background-color: #111111;
 }
-</script>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #6894c0;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #6894c0;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
